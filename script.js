40bf47d9945e6a84ec78f6afc4e4e89fe38d3023
Andrey Deev <adeev@aidriller.com>
2023-11-10 16:30:00 +0700

    #

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 async function getResponce(){
-    let response = await fetch("http://localhost:5000/user")
+    let response = await fetch("http://77.222.37.119:5000/api/users/", {
+        method: "GET",
+        headers: { "Accept": "application/json" }
+    });
     let content = await response.json()
     console.log(content)
 
     const select1 = document.querySelector('.select-1');
     let index = 0 
-    for (let id in content){
+    for (let id in content['users']){
         const option = document.createElement('option');
-        option.value = content[index]["name"];
-        option.text = content[index]["name"];
+        option.value = content['users'][index]["name"];
+        option.text = content['users'][index]["name"];
         select1.append(option);
         index+=1
         console.log(index)
     }
 }
-
+//console.log(getResponce())
 getResponce()
